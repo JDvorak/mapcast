1cdde4e10559fe44d9a647f7b561dc69ebf7f3b6
John Dvorak <john@amarant.io>
2017-01-03 13:31:23 -0800

    (fix) Remove return statement, use each as inputs.

--- components/walk.js
+++ components/walk.js
@@ -2,13 +2,11 @@ var _ = require('lodash')
 var traverse = require('traverse')
 
 module.exports = function makeWalker (match, transform) {
-  return function useTraverse (obj) {
-    n = traverse(obj).map(function each (each) {
+  return function _walk (obj) {
+    return traverse(obj).map(function each (each) {
       if (this.notLeaf && match(each, this.path, obj)) {
-        this.update(transform(this.node, this.path, obj))
+        this.update(transform(each, this.path, obj))
       }
-      return each
     })
-    return n;
   }
 }
