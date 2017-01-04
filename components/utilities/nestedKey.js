9777382c97ec32c21fa31a8de43d110ddcfd5c4c
John Dvorak <john@amarant.io>
2017-01-03 16:21:05 -0800

    (refactor) Eliminate unnecessary iteration + make sequence intuitive

--- components/utilities/nestedKey.js
+++ components/utilities/nestedKey.js
@@ -5,45 +5,39 @@ var _ = require('lodash')
 function nestedKey (o, path, v, options) { 
   options = options || {}
   options.extend = options.extend || false;
-  return _.flattenDeep(dive(o, path[0], 0, []));
 
-  function dive (obj, key, index, keys) {
-    if (!key) {
-      var temp = {}
-      temp[keys.join('.')] = obj
-      return temp
-    }
+  return _.flattenDeep(_dive(o, path[0], 0, []));
 
-    var regexMatch = key.match(/\/([\s\S]*)\/(ig)*/);
-    
-    if (regexMatch) {
+  function _dive (obj, key, index, keys) {
+    var regexMatch, isMatch, found = [];
+
+    if (regexMatch = key.match(/\/([\s\S]*)\/(ig)*/)) {
       key = new RegExp(regexMatch[1], regexMatch[2])
     }
 
-    var isMatch = matcher(key, {strict: true});
-    var spread = [];
-    var found = []
-
+    isMatch = matcher(key, {strict: true});
 
     for (var prop in obj) {
       if (obj.hasOwnProperty(prop) && isMatch(prop)) {
-        if ((index === path.length-1) && v) {
-          if (options.extend && _.isObject(obj[prop])) {
-            obj[prop] = xtend(obj[prop], v)
+        if ((index === path.length-1)) {
+          if (v) {
+            if (options.extend && _.isObject(obj[prop])) {
+              obj[prop] = xtend(obj[prop], v)
+            } else {
+              obj[prop] = v;
+            }
           } else {
-            obj[prop] = v;
+            var temp = {}
+            temp[keys.concat(prop).join('.')] = obj[prop]
+            found.push(temp)
           }
+        } else {
+          found.push(_dive(obj[prop], path[index + 1], index + 1, keys.concat(prop)))
         }
-        spread.push(prop)
       }
     }
 
-    if (spread.length > 0) {
-      found = spread.map(function (prop) {
-        return dive(obj[prop], path[index + 1], index + 1, keys.concat(prop))
-      }).filter((ea) => (ea != null))
-      return found
-    } 
+    return found.filter((ea) => (ea != null))
   }
 }
 
